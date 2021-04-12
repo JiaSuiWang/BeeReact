4cf72b65cc3de54a6ef0a8d9979a026828ffedf6
Jiasui <dokio.wang@gmail.com>
2021-04-12 21:27:39 +0900

    fix: init vscode

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,18 +1,36 @@
-import React, { FC, useState } from 'react';
+/*
+ * @Descripttion: 
+ * @Author: xiao wang
+ * @Date: 2021-04-12 17:51:35
+ * @LastEditors: xiao wang
+ * @LastEditTime: 2021-04-12 21:26:59
+ * @FilePath: \react-typescript\src\components\Test\index.tsx
+ */
+
+
+
+import React, { FC, useEffect, useState } from 'react';
 
 
 const Test: FC = (): JSX.Element => {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
 }
 
 export default Test;
+
+
